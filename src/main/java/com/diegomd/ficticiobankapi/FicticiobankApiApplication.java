3414ced2a34635bdd6e12a15dd568c8c8805a6cc
Diego Medeiros <mdsidiego@gmail.com>
2023-10-28 12:37:51 -0300

    add project's base files

---
+++ src/main/java/com/diegomd/ficticiobankapi/FicticiobankApiApplication.java
@@ -0,0 +1,13 @@
+package com.diegomd.ficticiobankapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FicticiobankApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FicticiobankApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/diegomd/ficticiobankapi/FicticiobankApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diegomd.ficticiobankapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FicticiobankApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
